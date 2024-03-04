d3ee780629c15eab5f954e368bdef867d97a6b7e
Cristian6597 <cristianercolani.w@gmail.com>
2024-03-04 11:25:38 +0100

    Monday algorithm

---
+++ JS Algorithms/Exercises  JavaScript/mondayArray.js
@@ -0,0 +1,17 @@
+// Return an array consisting of the largest number from each provided sub-array. For simplicity, the provided array will contain exactly 4 sub-arrays.
+
+function largestOfFour(arr) {
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+      let max = arr[i][0];
+      for (let j = 1; j < arr[i].length; j++) {
+        if (arr[i][j] > max) {
+          max = arr[i][j];
+        }
+      }
+      result.push(max);
+    }
+    return result;
+  }
+  
+  console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
