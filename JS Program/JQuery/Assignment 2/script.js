d9d7a8b80c7f8a21bbe308bf4ab2222c908b135c
Cristian6597 <cristianercolani.w@gmail.com>
2024-03-07 10:21:44 +0100

    Assignment 2 JQuery

---
+++ JS Program/JQuery/Assignment 2/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+  $("#img1").click(function () {
+    $(this).attr("src", "1b.jpg");
+    $(this).addClass("imagineScale");
+  });
+});
+
+$(document).ready(function () {
+  $("#img2").click(function () {
+    $(this).attr("src", "2b.jpg");
+    $(this).addClass("imagineScale");
+  });
+});
+
+$(document).ready(function () {
+  $("#img3").click(function () {
+    $(this).attr("src", "3b.jpg");
+    $(this).addClass("imagineScale");
+  });
+});
+
+
